d8bd111523b68c1c4f3f705bb9e2fd1579c80bdd
Theo Pack Test <tf.pack@gmail.com>
2016-09-10 08:39:00 +0200

    Read image and return as base64

---
+++ docker-image.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	b64 "encoding/base64"
+	"fmt"
+	"io/ioutil"
+)
+
+func main() {
+	b, err := ioutil.ReadFile("image.png")
+	if err != nil {
+		panic(err)
+	}
+
+	sEnc := b64.StdEncoding.EncodeToString(b)
+	fmt.Println(sEnc)
+}
